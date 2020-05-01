7119a93ca8959558ab3d4b197109ea83e93003d1
pradeepkarki <pradeepkarki70@gmail.com>
2020-05-01 19:03:33 +0530

    setup project

--- src/test/java/examples/users/UsersRunner.java
+++ src/test/java/examples/users/UsersRunner.java
@@ -3,10 +3,10 @@ package examples.users;
 import com.intuit.karate.junit5.Karate;
 
 class UsersRunner {
-    
-    @Karate.Test
-    Karate testUsers() {
-        return Karate.run("users").relativeTo(getClass());
-    }    
+
+	@Karate.Test
+	Karate testUsers() {
+		return Karate.run("users").relativeTo(getClass());
+	}
 
 }
